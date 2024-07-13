ba60b55a5c18e85d54f8ff1aede14473ed303351
arefinite <arefinite@gmail.com>
2024-07-13 23:53:47 +0600

    deploy

--- server/src/api/app.ts
+++ server/src/api/app.ts
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, '../../../client/dist')))
 
 // Handle React routing, return all requests to React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'))
+  res.sendFile(path.join(__dirname, '../../../client/dist', 'index.html'))
 })
 //routes
 
